6d1f2deae93e7113090a045d5461f2614860a26c
Dylan Marcus <dylan@mach1studios.com>
2020-01-21 11:45:31 -0500

    moved OSC to main fragment and added some input UI (WIP)

--- basic-example/src/main/java/com/bose/ar/basic_example/MainActivity.java
+++ basic-example/src/main/java/com/bose/ar/basic_example/MainActivity.java
@@ -10,12 +10,6 @@ import android.widget.Toast;
 
 import com.bose.wearable.BoseWearable;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import java.net.*;
-import java.util.*;
-import com.illposed.osc.*;
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
@@ -27,8 +21,6 @@ public class MainActivity extends AppCompatActivity {
                 Toast.LENGTH_LONG)
                 .show();
             finish();
-            // Start the thread that sends messages
-            oscThread.start();
             return;
         }
 
@@ -44,94 +36,4 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
         }
     }
-
-    /* These two variables hold the IP address and port number.
-     * You should change them to the appropriate address and port.
-     */
-    private String myIP = "12.34.56.78";
-    private int myPort = 12345;
-
-    // This is used to send messages
-    private OSCPortOut oscPortOut;
-
-    // This thread will contain all the code that pertains to OSC
-    private Thread oscThread = new Thread() {
-        @Override
-        public void run() {
-            /* The first part of the run() method initializes the OSCPortOut for sending messages.
-             *
-             * For more advanced apps, where you want to change the address during runtime, you will want
-             * to have this section in a different thread, but since we won't be changing addresses here,
-             * we only have to initialize the address once.
-             */
-
-            try {
-                // Connect to some IP address and port
-                oscPortOut = new OSCPortOut(InetAddress.getByName(myIP), myPort);
-            } catch(UnknownHostException e) {
-                // Error handling when your IP isn't found
-                return;
-            } catch(Exception e) {
-                // Error handling for any other errors
-                return;
-            }
-
-
-            /* The second part of the run() method loops infinitely and sends messages every 500
-             * milliseconds.
-             */
-            while (true) {
-                if (oscPortOut != null) {
-                    // Creating the message
-                    Object[] thingsToSend = new Object[3];
-                    thingsToSend[0] = "Hello World";
-                    thingsToSend[1] = 12345;
-                    thingsToSend[2] = 1.2345;
-
-                    /* The version of JavaOSC from the Maven Repository is slightly different from the one
-                     * from the download link on the main website at the time of writing this tutorial.
-                     *
-                     * The Maven Repository version (used here), takes a Collection, which is why we need
-                     * Arrays.asList(thingsToSend).
-                     *
-                     * If you're using the downloadable version for some reason, you should switch the
-                     * commented and uncommented lines for message below
-                     */
-                    OSCMessage message = new OSCMessage(myIP, Arrays.asList(thingsToSend));
-                    // OSCMessage message = new OSCMessage(myIP, thingsToSend);
-
-
-                    /* NOTE: Since this version of JavaOSC uses Collections, we can actually use ArrayLists,
-                     * or any other class that implements the Collection interface. The following code is
-                     * valid for this version.
-                     *
-                     * The benefit of using an ArrayList is that you don't have to know how much information
-                     * you are sending ahead of time. You can add things to the end of an ArrayList, but not
-                     * to an Array.
-                     *
-                     * If you want to use this code with the downloadable version, you should switch the
-                     * commented and uncommented lines for message2
-                     */
-                    ArrayList<Object> moreThingsToSend = new ArrayList<Object>();
-                    moreThingsToSend.add("Hello World2");
-                    moreThingsToSend.add(123456);
-                    moreThingsToSend.add(12.345);
-
-                    OSCMessage message2 = new OSCMessage(myIP, moreThingsToSend);
-                    //OSCMessage message2 = new OSCMessage(myIP, moreThingsToSend.toArray());
-
-                    try {
-                        // Send the messages
-                        oscPortOut.send(message);
-                        oscPortOut.send(message2);
-
-                        // Pause for half a second
-                        sleep(500);
-                    } catch (Exception e) {
-                        // Error handling for some error
-                    }
-                }
-            }
-        }
-    };
 }
--- basic-example/src/main/java/com/bose/ar/basic_example/MainFragment.java
+++ basic-example/src/main/java/com/bose/ar/basic_example/MainFragment.java
@@ -33,6 +33,12 @@ import com.bose.wearable.sensordata.SensorValue;
 import com.bose.wearable.sensordata.Vector;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.net.*;
+import java.util.*;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.illposed.osc.*;
+
 import java.util.Locale;
 
 public class MainFragment extends Fragment {
@@ -51,15 +57,22 @@ public class MainFragment extends Fragment {
     private TextView mPitch;
     private TextView mRoll;
     private TextView mYaw;
-    private TextView mX;
-    private TextView mY;
-    private TextView mZ;
+
+    public TextInputEditText mOscAddressInput;
+    public TextInputEditText mOscPortInput;
+    public boolean mYawEnabled;
+    public boolean mPitchEnabled;
+    public boolean mRollEnabled;
+
     @Nullable
     private Snackbar mSnackBar;
 
     @Override
     public void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Start the thread that sends messages
+        oscThread.start();
 
         final Bundle args = getArguments();
         if (args != null) {
@@ -71,6 +84,9 @@ public class MainFragment extends Fragment {
             throw new IllegalArgumentException();
         }
     }
+
+    // This is used to send messages
+    private OSCPortOut oscPortOut;
 
     @Nullable
     @Override
@@ -89,10 +105,33 @@ public class MainFragment extends Fragment {
         mPitch = view.findViewById(R.id.pitch);
         mRoll = view.findViewById(R.id.roll);
         mYaw = view.findViewById(R.id.yaw);
+        mOscAddressInput = view.findViewById(R.id.oscAddressInput);
+        mOscPortInput = view.findViewById(R.id.oscPortInput);
+        mYawEnabled = view.findViewById(R.id.yawEnable);
+        mPitchEnabled = view.findViewById(R.id.pitchEnable);
+        mRollEnabled = view.findViewById(R.id.rollEnable);
+        String myIP = mOscAddressInput.getText().toString();
+        int myPort = Integer.parseInt(mOscPortInput.getText().toString());
 
-        mX = view.findViewById(R.id.x);
-        mY = view.findViewById(R.id.y);
-        mZ = view.findViewById(R.id.z);
+        mOscAddressInput.setOnFocusChangeListener(this);
+        mOscPortInput.setOnFocusChangeListener(this);
+
+        @Override
+        public void onFocusChange(View view, boolean hasFocus) {
+
+            // When focus is lost check that the text field has valid values.
+
+            if (!hasFocus) {
+                switch (view.getId()) {
+                    case R.id.oscAddressInput:
+                        // Validate EditText1
+                        break;
+                    case R.id.oscPortInput:
+                        // Validate EditText2
+                        break;
+                }
+            }
+        }
     }
 
     @Override
@@ -187,9 +226,6 @@ public class MainFragment extends Fragment {
     @SuppressWarnings("PMD.ReplaceVectorWithList") // PMD confuses SDK Vector with java.util.Vector
     private void onAccelerometerData(@NonNull final SensorValue sensorValue) {
         final Vector vector = sensorValue.vector();
-        mX.setText(formatValue(vector.x()));
-        mY.setText(formatValue(vector.y()));
-        mZ.setText(formatValue(vector.z()));
     }
 
     private void onRotationData(@NonNull final SensorValue sensorValue) {
@@ -198,6 +234,8 @@ public class MainFragment extends Fragment {
         mPitch.setText(formatAngle(quaternion.xRotation()));
         mRoll.setText(formatAngle(-quaternion.yRotation()));
         mYaw.setText(formatAngle(-quaternion.zRotation()));
+
+
     }
 
     private void showError(final String message) {
@@ -219,4 +257,64 @@ public class MainFragment extends Fragment {
         final double degrees = radians * 180 / Math.PI;
         return String.format(Locale.US, "%.2f°", degrees);
     }
+
+    // This thread will contain all the code that pertains to OSC
+    private Thread oscThread = new Thread() {
+        @Override
+        public void run() {
+            /* The first part of the run() method initializes the OSCPortOut for sending messages.
+             *
+             * For more advanced apps, where you want to change the address during runtime, you will want
+             * to have this section in a different thread, but since we won't be changing addresses here,
+             * we only have to initialize the address once.
+             */
+
+            try {
+                // Connect to some IP address and port
+                oscPortOut = new OSCPortOut(InetAddress.getByName(myIP), myPort);
+            } catch(UnknownHostException e) {
+                // Error handling when your IP isn't found
+                return;
+            } catch(Exception e) {
+                // Error handling for any other errors
+                return;
+            }
+
+
+            /* The second part of the run() method loops infinitely and sends messages every 10
+             * milliseconds.
+             */
+            while (true) {
+                if (oscPortOut != null) {
+                    // Creating the message
+                    Object[] orientationOSC = new Object[3];
+                    orientationOSC[0] = mYaw;
+                    orientationOSC[1] = mPitch;
+                    orientationOSC[2] = mRoll;
+
+                    /* The version of JavaOSC from the Maven Repository is slightly different from the one
+                     * from the download link on the main website at the time of writing this tutorial.
+                     *
+                     * The Maven Repository version (used here), takes a Collection, which is why we need
+                     * Arrays.asList(thingsToSend).
+                     *
+                     * If you're using the downloadable version for some reason, you should switch the
+                     * commented and uncommented lines for message below
+                     */
+                    OSCMessage message = new OSCMessage(myIP, Arrays.asList(orientationOSC));
+                    // OSCMessage message = new OSCMessage(myIP, orientationOSC);
+
+                    try {
+                        // Send the messages
+                        oscPortOut.send(message);
+
+                        // Pause for half a second
+                        sleep(10);
+                    } catch (Exception e) {
+                        // Error handling for some error
+                    }
+                }
+            }
+        }
+    };
 }
